7f6b8c8b85620661413e0a88f6ffed5c8ea50929
Ricardo Gore <rjhgore@gmail.com>
2022-03-22 19:00:47 -0700

    trimmomatic dataviz

--- DataViz.R
+++ DataViz.R
@@ -1,16 +1,19 @@
-rm(list = ls())
+rm(list = ls()) # Limpiar la memoria de la sesion de R
 
-options(stringsAsFactors = FALSE)
+options(stringsAsFactors = FALSE) # 
 
 library(tidyverse)
+library(patchwork)
 
 # Define Color Palette: ----
 
 colNames <- c("Both Surviving",	"Forward Only",	"Reverse Only",	"Dropped")
 
-getPalette <- RColorBrewer::brewer.pal(length(colNames), 'Paired')
+RColorBrewer::display.brewer.all() # Revisamos todas las paletas de colores que tenemos
 
-Cvalues <- structure(getPalette, names = colNames) 
+getPalette <- RColorBrewer::brewer.pal(length(colNames), 'Set1')
+
+Cvalues <- structure(getPalette, names = colNames)
 
 path <- '~/Documents/GitHub/Cancer_sete_T_assembly/'
 
@@ -20,11 +23,13 @@ file <- '.csv$'
 
 file <- list.files(path, pattern = file, full.names = TRUE)
 
-df <- lapply(file, read.table)
+df <- lapply(file, read.table) # leer todas las tablas
 
 df <- do.call(rbind, df)
 
 colnames(df) <- c('id', 'Input Read Pairs',colNames)
+
+head(df)
 
 # basic plot ----
 
@@ -32,7 +37,7 @@ barplot(df$`Input Read Pairs`)
 
 barplot(sort(df$`Input Read Pairs`))
 
-#
+# Algo publicable
 
 df %>%
   pivot_longer(cols = all_of(colNames)) %>%
@@ -63,7 +68,8 @@ df_longer %>%
 p2
 
 # Proficient
-caption <- ' ILLUMINACLIP:TruSeq3-PE-2.fa:2:30:10 SLIDINGWINDOW:4:15 MINLEN:36 LEADING:5 TRAILING:5'
+
+caption <- 'ILLUMINACLIP:TruSeq3-PE-2.fa:2:30:10 SLIDINGWINDOW:4:15 MINLEN:36 LEADING:5 TRAILING:5'
 
 p2 +
   labs(caption = caption) +
@@ -72,7 +78,9 @@ p2 +
     legend.position = 'top',
     legend.text = element_text(size = 5),
     strip.background = element_blank(),
-    panel.border = element_blank()) -> p2
+    panel.border = element_blank()) -> p3
+
+p3
 
 # Percentage
 
@@ -81,21 +89,28 @@ df %>%
       .fns = ~./`Input Read Pairs`)) %>% 
   pivot_longer(cols = all_of(colNames)) %>%
   mutate(name = factor(name, levels = colNames)) %>%
-  arrange(desc(value)) %>%
+  # arrange(desc(value)) %>%
   group_by(name) %>%
   mutate(id = forcats::fct_reorder(id, `Input Read Pairs`)) %>%
   ggplot(aes(x = id, y = value*100, fill = name)) +
   geom_col(position = position_stack(reverse = TRUE)) +
-  labs(y = '%', x = 'Sample') +
+  labs(y = '%', x = 'Sample', 
+    subtitle = caption) +
   coord_flip() +
   scale_fill_manual('', values = Cvalues) +
   theme_bw(base_size = 10, base_family = "GillSans") +
   theme(
-    legend.position = 'none',
+    legend.position = 'bottom',
     legend.text = element_text(size = 5),
     strip.background = element_blank(),
-    panel.border = element_blank()) -> p3
+    panel.border = element_blank()) -> p4
+
+p4
+
+ggsave(p4, filename = 'trimmomatic_pct.png', path = path, 
+  width = 7, height = 10)
 
 library(patchwork)
 
-p1+p2+p3
+p1+p2+p3+p4
+
